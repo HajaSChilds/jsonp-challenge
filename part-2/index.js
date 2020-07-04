bc8968eaaad257181d6f1f9aa145a30235a44881
HajaSC <hajasc@protonmail.com>
2020-07-04 04:06:16 -0400

    Adds more CSS styles, refactors functions

--- part-2/index.js
+++ part-2/index.js
@@ -1,6 +1,6 @@
 
-// Load data from json
 
+// Load data from json
 function getWeatherData(url) {
 
     //Add url to head tag
@@ -17,122 +17,133 @@ function getWeatherData(url) {
 
 
     // Add callback script to body tag
-    function addScript(url) {
+    function addScript() {
         const body = document.body;
         const script1 = document.createElement("script");
 
         // Add callback function to script tag
         script1.innerHTML =  hdnWeatherJsonpCallback; 
         body.appendChild(script1);
-    
+        body.removeChild(script1);
     }
 
-    addScript(url);
-
-    
+    addScript();
     
 
-    //Define named callback function    
+    //Define jsonp callback function    
     function hdnWeatherJsonpCallback(data) {
 
-      function loadCities() {
+      //add cities to dropdown menu  
+        function loadCities() {
     
-        let cityArray = data.cities;
-        console.log(cityArray); 
+            let cityArray = data.cities;
+            console.log(cityArray); 
 
-        cityArray.forEach((city)=> {
-            console.log(city.geoloc.city);
+            cityArray.forEach((city)=> {
+                console.log(city.geoloc.city);
+                let cityName = city.geoloc.city;
+                console.log("index: ", cityArray.indexOf(city))
+                let index = cityArray.indexOf(city)
+                const cityNode = document.createElement('option');
 
-            console.log("index: ", cityArray.indexOf(city))
+                cityNode.setAttribute("value", index);
 
-            const cityNode = document.createElement('option');
+                //shows Palo Alto as default
+                if(index == 9) {
+                cityNode.setAttribute("selected", "selected");  
+                } 
 
-            cityNode.setAttribute(value, cityArray.indexOf(city));
+                cityNode.innerHTML = cityName;
 
-            dropdown = document.getElementById("cities");
-
-            dropdown.appendChild(cityNode);
+                dropdown = document.getElementById("cities");
+                dropdown.appendChild(cityNode);
+                valueCity = dropdown.value;
+            })
+            
         }
-          )
-          
-      }
      
-      //loadCities();
-
-      // Define variable to select city
-      let whichCity = 10;
-
-	  let paloAltoWeatherCurrentDay = data.cities[whichCity].current[0];
-	  
-	  let paloAltoWeatherWeekly = data.cities[whichCity];
-
-
-	  let currentTemp = paloAltoWeatherCurrentDay.temp + "&#176";
-	  let currentCondition = paloAltoWeatherCurrentDay.condition;
-	  let fiveDays = paloAltoWeatherWeekly.weekly;
-	   
-	  fiveDays.length = 5; // To only take five days of data
-
-
-
-	  //Current Weather Conditions
-
-	  // Assumption:  Part of the challenge is that I shouldn't just alter the html to hardcode this in
-
-	  const node = document.createElement("div");
-	  node.innerHTML = currentTemp;
-	 
-	  const node2 = document.createElement("div"); 
-	  node2.innerHTML = currentCondition;
-
-
-	  document.getElementById('current').appendChild(node);
-	  document.getElementById('current').appendChild(node2);	
-	  
-
-
-	  //Weekly Forecast Conditions
-
-	  //Assumption: I shouldn't alter the html to hardcode this weekly forecast in
-
-	  //Assumption:  I should place the weekly forecast into the "forecast" div
-
-	  const forecastNode = document.createElement("div");
-	
-	  
-     // Put each day of the forecast data into its own div for later styling 
-	  fiveDays.map((day,index) => {
-			let nextDay = paloAltoWeatherWeekly.weekly[index].day;
-		
-
-			let nextCondition = paloAltoWeatherWeekly.weekly[index].daycondition;
-		
-		
-			const newDay = document.createElement("div");//Parent div
-
-			const days = document.createElement("p"); //Child div
-			const condition = document.createElement("p");
-
-			days.innerHTML = nextDay;
-			condition.innerHTML = nextCondition;
-
-			newDay.appendChild(days);
-			newDay.appendChild(condition);
-			
-			//Add all days to one div for display
-			forecastNode.appendChild(newDay);
-
-	  }, 2);
-
-
-	  // Add weekly forecast data to display
-	  document.getElementById("forecast").appendChild(forecastNode);
-
+      loadCities();
+      document
+        .getElementById("cities")
+        .addEventListener("onchange", displayWeather); 
+      
+        displayWeather(data, valueCity);
+       
+      
+      // Define variable for selected city
+       //Changes the data when the dropdown menu changes
+      let whichCity = valueCity;
+      console.log("whichCity: ", whichCity);
+    //Changes the data when the dropdown menu changes
+    
+        
     }
-
     
 }
+
+function displayWeather(data,whichCity) {
+  let weatherCurrentDay = data.cities[whichCity].current[0];
+
+  let weatherWeekly = data.cities[whichCity];
+
+  let currentTemp = weatherCurrentDay.temp + "&#176";
+  let currentCondition = weatherCurrentDay.condition;
+  let fiveDays = weatherWeekly.weekly;
+
+  fiveDays.length = 5; // To only take five days of data
+
+
+  //Current Weather Conditions
+
+  // Assumption:  Part of the challenge is that I shouldn't just alter the html to hardcode this in
+
+  const node = document.createElement("div");
+  node.innerHTML = currentTemp;
+
+  const node2 = document.createElement("div");
+  node2.innerHTML = currentCondition;
+
+  document.getElementById("current").appendChild(node);
+  document.getElementById("current").appendChild(node2);
+
+  //Weekly Forecast Conditions
+
+  //Assumption: I shouldn't alter the html to hardcode this weekly forecast in
+
+  //Assumption:  I should place the weekly forecast into the "forecast" div
+
+  const forecastNode = document.createElement("div");
+
+  // Put each day of the forecast data into its own div for later styling option
+  fiveDays.map((day, index) => {
+    let nextDay = weatherWeekly.weekly[index].day;
+
+    let nextCondition = weatherWeekly.weekly[index].daycondition;
+
+    const newDay = document.createElement("div"); //Parent div
+
+    const days = document.createElement("p"); //Child div
+    const condition = document.createElement("p");
+
+    days.innerHTML = nextDay;
+    condition.innerHTML = nextCondition;
+
+    newDay.appendChild(days);
+    newDay.appendChild(condition);
+
+    //Add all days to one div for display
+    forecastNode.appendChild(newDay);
+  }, 2);
+
+  // Add weekly forecast data to display
+  document.getElementById("forecast").appendChild(forecastNode);
+  
+}
+
+
+
 
 getWeatherData(
   "https://www.sfchronicle.com/external/weather/weather.json?callback=hdnWeatherJsonpCallback"
 );
+
